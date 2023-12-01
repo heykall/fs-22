1224267114a87e88780e2ef452032c2beb24c728
heykall <muh.heykall@gmail.com>
2023-12-01 14:03:26 +0700

    fix again

--- src/pages/Halaman-Buku/Buku.jsx
+++ src/pages/Halaman-Buku/Buku.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import CardBuku from "../../components/CardBuku";
-import styles from "./BukyStyle.module.css";
+import styles from "./BukuStyle.module.css";
 
 export default function Buku() {
   // handle card ditambahkan, dari api, data dari 2 paling belakang
